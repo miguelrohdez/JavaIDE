4359de99ee2fb4b6eafd6cfd0bc4b639e474dd83
Miguel Rojas <crayzy_1_9@hotmail.com>
2020-06-26 02:52:05 -0500

    Entregable1

---
+++ src/com/miguelrohdez/Entregable1/Entregable1.java
@@ -0,0 +1,35 @@
+package com.miguelrohdez.Entregable1;
+
+public class Entregable1 {
+    public static void main(String[] args) {
+        int[][] matriz2D;
+        matriz2D = new int[4][4];
+        int i;
+        int j;
+        int c;
+        int k;
+        int l;
+        c = 2;
+        for (i = 0; i <matriz2D.length ; i++) {
+            for (j = 0; j <matriz2D.length ; j++) {
+                //System.out.println("i= "+i+"\tj= " + j + " => "+c);
+                System.out.println((matriz2D[i][j] = c)+" ");
+                c=c+2;
+            }
+
+        }
+        System.out.println("\n----------------------------------------------------------------\n" +
+                "---------Matriz Impresa----------\n" +
+                "----------------------------------------------------------------\n");
+        for (k = 0; k <matriz2D.length ; k++) {
+            for (l = 0; l <matriz2D.length ; l++) {
+                System.out.print(matriz2D[k][l]+"\t");
+
+            }
+            System.out.println("");
+
+        }
+
+    }
+
+}
